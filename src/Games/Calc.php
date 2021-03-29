a08e343c5edb7e865a277e4145201c49e76aad80
Evgeniya M <web-developer@MacBook-Air-Evgeniya.local>
2021-03-29 22:56:20 +0700

    Calc fix

--- src/Games/Calc.php
+++ src/Games/Calc.php
@@ -6,7 +6,10 @@ use function cli\line;
 use function cli\prompt;
 
 require __DIR__ . '/../../vendor/autoload.php';
-require dirname(__DIR__) . '/Engine.php';
+
+line("Welcome to the Brain Game!");
+$name = prompt('May I have your name?');
+line('Hello, %s!', $name);
 
 line('What is the result of the expression?');
 $mathematicalOperation = ['+', '-', '*'];
